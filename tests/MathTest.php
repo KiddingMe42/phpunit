3811da7896bb2ab8c5ef2de356537edd7fa51388
Michael <contact@eewee.fr>
2018-01-06 17:57:34 +0100

    Sample
    Sample

--- tests/MathTest.php
+++ tests/MathTest.php
@@ -21,181 +21,181 @@ class MathTest extends TestCase
      */
     public function testMajeur()
     {
-    	$a = new \Eewee\Math();
-    	$this->assertFalse($a->majeur(10));
-    	$this->assertTrue($a->majeur(18));
-	    $this->assertTrue($a->majeur(25));
+        $a = new \Eewee\Math();
+        $this->assertFalse($a->majeur(10));
+        $this->assertTrue($a->majeur(18));
+        $this->assertTrue($a->majeur(25));
     }
 
     public function testContains()
     {
-	    // ok
-	    $this->assertContains(3, [1, 2, 3]);
-	    $this->assertContains('bar', 'foobar');
-	    $this->assertContains('foo', 'FooBar', '', true);
+        // ok
+        $this->assertContains(3, [1, 2, 3]);
+        $this->assertContains('bar', 'foobar');
+        $this->assertContains('foo', 'FooBar', '', true);
 
-	    // nok
-	    //$this->assertContains(4, [1, 2, 3]);
-	    //$this->assertContains('baz', 'foobar');
-	    //$this->assertContains('foo', 'FooBar');
+        // nok
+        //$this->assertContains(4, [1, 2, 3]);
+        //$this->assertContains('baz', 'foobar');
+        //$this->assertContains('foo', 'FooBar');
     }
-	//public function testContainsOnly()
-	//{
-		//nok
-		//$this->assertContainsOnly('string', ['1', '2', 3]);
-	//}
-	public function testContainsOnlyInstancesOf()
-	{
-		// ok
-		$this->assertContainsOnlyInstancesOf(
-			\Eewee\Math::class,
-			[new \Eewee\Math, new \Eewee\Math]
-		);
-		// nok
-		//$this->assertContainsOnlyInstancesOf(
-		//	\Eewee\Math::class,
-		//	[new \Eewee\Math, new Bar, new \Eewee\Math]
-		//);
-	}
-	public function testCount()
-	{
-		//ok
-		$this->assertCount(1, ['foo']);
-		// nok
-		//$this->assertCount(0, ['foo']);
-	}
-	public function testDirectoryExists()
-	{
-		// ok
-		$this->assertDirectoryExists('./vendor');
-		// nok
-		//$this->assertDirectoryExists('./aaa');
-	}
-	public function testDirectory()
-	{
-		// ok
-		$this->assertDirectoryIsReadable('./src');
-	}
-	public function testDirectoryIsWritable()
-	{
-		// ok
-		$this->assertDirectoryIsWritable('./src');
-	}
-	public function testEmpty()
-	{
-		// ok
-		$a = "";
-		$this->assertEmpty($a);
-		// nok
-		//$this->assertEmpty(['foo']);
-	}
-	// A faire : assertEqualXMLStructure
-	public function testEquals()
-	{
-		// ok
-		$this->assertEquals(1, 1);
-		$this->assertEquals('bar', 'bar');
-		// Ecart entre 1.0 et 1.1 < à 0.2
-		$this->assertEquals(1.0, 1.1, '', 0.2);
+    //public function testContainsOnly()
+    //{
+    //nok
+    //$this->assertContainsOnly('string', ['1', '2', 3]);
+    //}
+    public function testContainsOnlyInstancesOf()
+    {
+        // ok
+        $this->assertContainsOnlyInstancesOf(
+            \Eewee\Math::class,
+            [new \Eewee\Math, new \Eewee\Math]
+        );
+        // nok
+        //$this->assertContainsOnlyInstancesOf(
+        //	\Eewee\Math::class,
+        //	[new \Eewee\Math, new Bar, new \Eewee\Math]
+        //);
+    }
+    public function testCount()
+    {
+        //ok
+        $this->assertCount(1, ['foo']);
+        // nok
+        //$this->assertCount(0, ['foo']);
+    }
+    public function testDirectoryExists()
+    {
+        // ok
+        $this->assertDirectoryExists('./vendor');
+        // nok
+        //$this->assertDirectoryExists('./aaa');
+    }
+    public function testDirectory()
+    {
+        // ok
+        $this->assertDirectoryIsReadable('./src');
+    }
+    public function testDirectoryIsWritable()
+    {
+        // ok
+        $this->assertDirectoryIsWritable('./src');
+    }
+    public function testEmpty()
+    {
+        // ok
+        $a = "";
+        $this->assertEmpty($a);
+        // nok
+        //$this->assertEmpty(['foo']);
+    }
+    // A faire : assertEqualXMLStructure
+    public function testEquals()
+    {
+        // ok
+        $this->assertEquals(1, 1);
+        $this->assertEquals('bar', 'bar');
+        // Ecart entre 1.0 et 1.1 < à 0.2
+        $this->assertEquals(1.0, 1.1, '', 0.2);
 
-		// nok
-		//$this->assertEquals(1, 0);
-		//$this->assertEquals('bar', 'baz');
-		// Ecart entre 1.0 et 1.1 < à 0.2
-		//$this->assertEquals(1.0, 1.1, '', 0.05);
-	}
-	//public function testEquals2()
-	//{
-		// nok
-		//$expected = new DOMDocument;
-		//$expected->loadXML('<foo><bar/></foo>');
-		//
-		//$actual = new DOMDocument;
-		//$actual->loadXML('<bar><foo/></bar>');
-		//
-		//$this->assertEquals($expected, $actual);
-	//}
-	public function testEquals3()
-	{
-		// ok
-		$expected = new \stdClass;
-		$expected->foo = 'foo';
-		$expected->bar = 'bar';
+        // nok
+        //$this->assertEquals(1, 0);
+        //$this->assertEquals('bar', 'baz');
+        // Ecart entre 1.0 et 1.1 < à 0.2
+        //$this->assertEquals(1.0, 1.1, '', 0.05);
+    }
+    //public function testEquals2()
+    //{
+    // nok
+    //$expected = new DOMDocument;
+    //$expected->loadXML('<foo><bar/></foo>');
+    //
+    //$actual = new DOMDocument;
+    //$actual->loadXML('<bar><foo/></bar>');
+    //
+    //$this->assertEquals($expected, $actual);
+    //}
+    public function testEquals3()
+    {
+        // ok
+        $expected = new \stdClass;
+        $expected->foo = 'foo';
+        $expected->bar = 'bar';
 
-		$actual = new \stdClass;
-		$actual->foo = 'foo';
-		$actual->bar = 'bar';
+        $actual = new \stdClass;
+        $actual->foo = 'foo';
+        $actual->bar = 'bar';
 
-		$this->assertEquals($expected, $actual);
+        $this->assertEquals($expected, $actual);
 
-		// nok
-		//$expected = new stdClass;
-		//$expected->foo = 'foo';
-		//$expected->bar = 'bar';
+        // nok
+        //$expected = new stdClass;
+        //$expected->foo = 'foo';
+        //$expected->bar = 'bar';
 
-		//$actual = new stdClass;
-		//$actual->foo = 'bar';
-		//$actual->baz = 'bar';
+        //$actual = new stdClass;
+        //$actual->foo = 'bar';
+        //$actual->baz = 'bar';
 
-		//$this->assertEquals($expected, $actual);
-	}
-	public function testEquals4()
-	{
-		// ok
-		$this->assertEquals(['a', 'b', 'c'], ['a', 'b', 'c']);
-		// nok
-		//$this->assertEquals(['a', 'b', 'c'], ['a', 'c', 'd']);
-	}
-	public function testFalse()
-	{
-		// ok
-		$this->assertFalse(false);
-		// nok
-		//$this->assertFalse(true);
-	}
-	public function testFileEquals()
-	{
-		// ok
-		$this->assertFileEquals('./index.php', './index.php');
-		// nok
-		//$this->assertFileEquals('./index.php', './index2.php');
-	}
-	public function testFileExists()
-	{
-		// ok
-		$this->assertFileExists('./index.php');
-		// nok
-		//$this->assertFileExists('./index2.php');
-	}
-	public function testFileIsReadable()
-	{
-		// ok
-		$this->assertFileIsReadable('./index.php');
-		// nok
-		//$this->assertFileIsReadable('./index2.php');
-	}
-	public function testFileIsWritable()
-	{
-		// ok
-		$this->assertFileIsWritable('./index.php');
-		// nok
-		//$this->assertFileIsWritable('./index2.php');
-	}
-	public function testGreaterThan()
-	{
-		// ok
-		$this->assertGreaterThan(1, 2);
-		// nok
-		//$this->assertGreaterThan(2, 1);
-	}
-	public function testGreaterThanOrEqual()
-	{
-		// ok
-		$this->assertGreaterThanOrEqual(2, 2);
-		$this->assertGreaterThanOrEqual(1, 2);
-		// nok
-		//$this->assertGreaterThanOrEqual(2, 1);
-	}
+        //$this->assertEquals($expected, $actual);
+    }
+    public function testEquals4()
+    {
+        // ok
+        $this->assertEquals(['a', 'b', 'c'], ['a', 'b', 'c']);
+        // nok
+        //$this->assertEquals(['a', 'b', 'c'], ['a', 'c', 'd']);
+    }
+    public function testFalse()
+    {
+        // ok
+        $this->assertFalse(false);
+        // nok
+        //$this->assertFalse(true);
+    }
+    public function testFileEquals()
+    {
+        // ok
+        $this->assertFileEquals('./index.php', './index.php');
+        // nok
+        //$this->assertFileEquals('./index.php', './index2.php');
+    }
+    public function testFileExists()
+    {
+        // ok
+        $this->assertFileExists('./index.php');
+        // nok
+        //$this->assertFileExists('./index2.php');
+    }
+    public function testFileIsReadable()
+    {
+        // ok
+        $this->assertFileIsReadable('./index.php');
+        // nok
+        //$this->assertFileIsReadable('./index2.php');
+    }
+    public function testFileIsWritable()
+    {
+        // ok
+        $this->assertFileIsWritable('./index.php');
+        // nok
+        //$this->assertFileIsWritable('./index2.php');
+    }
+    public function testGreaterThan()
+    {
+        // ok
+        $this->assertGreaterThan(1, 2);
+        // nok
+        //$this->assertGreaterThan(2, 1);
+    }
+    public function testGreaterThanOrEqual()
+    {
+        // ok
+        $this->assertGreaterThanOrEqual(2, 2);
+        $this->assertGreaterThanOrEqual(1, 2);
+        // nok
+        //$this->assertGreaterThanOrEqual(2, 1);
+    }
     //    public function testInfinite()
 //    {
 //        // nok
@@ -294,10 +294,10 @@ class MathTest extends TestCase
         // FORMAT :
         // https://phpunit.de/manual/current/fr/appendixes.assertions.html#appendixes.assertions.assertStringMatchesFormat
 
-    	// ok
-	    $this->assertStringMatchesFormat('%s', 'foo');
-	    // nok
-	    //$this->assertStringMatchesFormat('%i', 'foo');
+        // ok
+        $this->assertStringMatchesFormat('%s', 'foo');
+        // nok
+        //$this->assertStringMatchesFormat('%i', 'foo');
     }
     // assertStringMatchesFormatFile()
     public function testSame()
@@ -350,6 +350,17 @@ class MathTest extends TestCase
         //);
     }
 
+    public function testExpectFooActualFoo()
+    {
+        $this->expectOutputString('foo');
+        print 'foo';
+    }
+    //public function testExpectBarActualBaz()
+    //{
+    //	$this->expectOutputString('bar');
+    //	print 'baz';
+    //}
+
     //---------------------------------------------
     // OTHER
     //---------------------------------------------
@@ -361,7 +372,7 @@ class MathTest extends TestCase
 
         // Config
         $stub->method('majeur')
-             ->willReturn('non');
+            ->willReturn('non');
 
         // Test
         $this->assertEquals('non', $stub->majeur(10));
@@ -372,7 +383,7 @@ class MathTest extends TestCase
         $stub = $this->createMock(\Eewee\Math::class);
         // return 1er arg
         $stub->method('majeur')
-             ->willReturnArgument(0);
+            ->willReturnArgument(0);
         //   ->will($this->returnArgument(0));
 
         $this->assertEquals(10, $stub->majeur(10));
@@ -384,7 +395,7 @@ class MathTest extends TestCase
         $stub = $this->createMock(\Eewee\Math::class);
         // return l'objet
         $stub->method('majeur')
-             ->will($this->returnSelf());
+            ->will($this->returnSelf());
 
         $this->assertSame($stub, $stub->majeur(50));
     }
@@ -407,7 +418,7 @@ class MathTest extends TestCase
     {
         $stub = $this->createMock(\Eewee\Math::class);
         $stub->method('majeur')
-             ->will($this->onConsecutiveCalls('majeur', 'mineur'));
+            ->will($this->onConsecutiveCalls('majeur', 'mineur'));
         $this->assertEquals('majeur', $stub->majeur(5));
         $this->assertEquals('mineur', $stub->majeur(7));
     }
@@ -426,23 +437,68 @@ class MathTest extends TestCase
             ->setMethods(['set'])
             ->getMock();
 
-            $mock->expects($this->exactly(2))
+        $mock->expects($this->exactly(2))
             ->method('set')
             ->withConsecutive(
-            [$this->equalTo('foo'), $this->greaterThan(0)],
-            [$this->equalTo('bar'), $this->greaterThan(0)]
+                [$this->equalTo('foo'), $this->greaterThan(0)],
+                [$this->equalTo('bar'), $this->greaterThan(0)]
             );
 
-            $mock->set('foo', 21);
-            $mock->set('bar', 48);
+        $mock->set('foo', 21);
+        $mock->set('bar', 48);
     }
-    
 
-    //    public function bbb()
-//    {
-//        $this->formatter->expects( $this->any() )
-//            ->method( 'currencyAmt' )
-//            ->with( $this->anything() )
-//            ->will( $this->returnArgument( 0 ) );
-//    }
+    //---------------------------------------------
+    // METHODE AUTOMATIQUE
+    // Source : https://phpunit.de/manual/current/fr/fixtures.html
+    //---------------------------------------------
+    /*
+    public static function setUpBeforeClass()
+    {
+        fwrite(STDOUT, __METHOD__ . "\n");
+    }
+
+    protected function setUp()
+    {
+        fwrite(STDOUT, __METHOD__ . "\n");
+    }
+
+    protected function assertPreConditions()
+    {
+        fwrite(STDOUT, __METHOD__ . "\n");
+    }
+
+    public function testOne()
+    {
+        fwrite(STDOUT, __METHOD__ . "\n");
+        $this->assertTrue(true);
+    }
+
+    public function testTwo()
+    {
+        fwrite(STDOUT, __METHOD__ . "\n");
+        $this->assertTrue(false);
+    }
+
+    protected function assertPostConditions()
+    {
+        fwrite(STDOUT, __METHOD__ . "\n");
+    }
+
+    protected function tearDown()
+    {
+        fwrite(STDOUT, __METHOD__ . "\n");
+    }
+
+    public static function tearDownAfterClass()
+    {
+        fwrite(STDOUT, __METHOD__ . "\n");
+    }
+
+    protected function onNotSuccessfulTest(Exception $e)
+    {
+        fwrite(STDOUT, __METHOD__ . "\n");
+        throw $e;
+    }
+    */
 }
